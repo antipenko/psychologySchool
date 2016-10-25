eb9a041a47817d8b02cddbc3b2146c7ba6441ef6
antipenko <sweetglory.victory@gmail.com>
2016-10-25 20:06:07 +0300

    add sections to home page

--- templates/template-home.php
+++ templates/template-home.php
@@ -57,46 +57,76 @@ $eventArgs = array(
 		 this sectin be able to change and in Wordpress too
 		 START BA-COURSE
 		-->
-		<section class="ba-sourse ba-section">
+		<section class="ba-course ba-section">
 			<div class="row">
 				<div class="columns medium-6">
 					<?php 
-						$image = get_field('course-img');
+					$image = get_field('course-img');
 					if( !empty($image) ): ?>
-						<img  class='ba-course__img' src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					<img  class='ba-course__img' src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 
-					<?php endif; ?>
-				</div>
-				
-				<div class="columns medium-6">
-					<h2 class="ba-course__title ba-title"> <?php the_field('course-name'); ?> </h2>
-					<?php the_field('course-description'); ?>
-					<a href=" <?php the_field('course-link'); ?> " class='button ba-course__button'>Читать дальше</a>
+				<?php endif; ?>
+			</div>
+
+			<div class="columns medium-6">
+				<h2 class="ba-course__title ba-title"> <?php the_field('course-name'); ?> </h2>
+				<?php the_field('course-description'); ?>
+				<a href=" <?php the_field('course-link'); ?> " class='button ba-course__button'>Читать дальше</a>
 			</div>
 		</div>
 	</section>
-
-	<!-- END BA-COURSE -->
-	<section class="ba-contacts ">
-		<div class="row ">
-			<h3 class="text-center"><?php the_field('contact_title') ?></h3>
-			<div class="columns text-center">
-				<?php the_field('contact_form') ?>
+	
+	<section class="ba-clients ba-section text-center">
+		<div class="row">
+			<div class="columns">
+				<h2 class="ba-title">
+					<?php the_field('clients'); ?>
+				</h2>
+				<?php the_field('clients-description'); ?>
+			
+			<!-- START DISPLAY LOGO CLIENTS  -->
+			<div class="ba-clients-list">
+				<?php if( have_rows('clients-repeater') ): ?>
+						<?php while( have_rows('clients-repeater') ): the_row(); 
+						$image = get_sub_field('client-logo');
+						?>
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+					<?php endwhile; ?>
+				<?php endif; ?>
 			</div>
+			<!-- END DISPLAY LOGO CLIENTS  -->
+
 		</div>
-	</section>
+	</div>
+</section>
+<!-- END BA-COURSE -->
+<section class="ba-contacts ba-section text-center">
+	<div class="row ">
+	<div class="columns">
+		<!-- <h3 class="text-center"><?php //the_field('contact_title') ?></h3>
+		<div class="columns text-center">
+			<?php //the_field('contact_form') ?>
+		</div> -->
 
-	<section class="ba-map">
-		<?php if( $location = get_field('map')):?>
-			<div class="acf-map">
-				<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
-			</div>
-		<?php endif; ?>
-	</section>
+		<h2 class="ba-title ba-contacts__title">
+			Check Out Our Monthly Promotions!
+		</h2>
+		<a href="#" class="button ba-contacts__button">Жми сюда</a>
+		</div>
+	</div>
+</section>
+
+<section class="ba-map">
+	<?php if( $location = get_field('map')):?>
+		<div class="acf-map">
+			<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+		</div>
+	<?php endif; ?>
+</section>
 
 
 
 
 
 
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
